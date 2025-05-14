db6345f229b10632864633d6e0348364ef954ec5
Iqmalmal <miqmaldaniel.zainal@student.gmi.edu.my>
2025-05-14 16:43:11 +0800

    initial
    PONS API

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get('/api/translate', async (req, res) => {
+  const { q, l } = req.query;
+  try {
+    const response = await axios.get('https://api.pons.com/v1/dictionary', {
+      headers: {
+        'X-Secret': process.env.PONS_SECRET
+      },
+      params: { q, l }
+    });
+    res.json(response.data);
+  } catch (err) {
+    res.status(err.response?.status || 500).json({ error: err.message });
+  }
+
+});
+
+
+
+app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
